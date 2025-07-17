990acc45c09a97cd2899eb46682c1289a832a776
JB2DEV <e.02.tucom.cdo@telefonica.com>
2025-07-17 14:06:58 +0200

    setup base hexagonal structure and reactive dependencies

---
+++ src/main/java/com/interview/materials/feature/test/inditex/InterviewMaterialsFeatureTestInditexApplication.java
@@ -0,0 +1,13 @@
+package com.interview.materials.feature.test.inditex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewMaterialsFeatureTestInditexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewMaterialsFeatureTestInditexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/interview/materials/feature/test/inditex/InterviewMaterialsFeatureTestInditexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interview.materials.feature.test.inditex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewMaterialsFeatureTestInditexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
